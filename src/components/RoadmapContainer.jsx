7f477ead464261c48d6cc74e811871fd560c0ff6
Ewan Porter <porter.ewan@mail.com>
2022-07-18 13:30:08 +0100

    roadmap edit

--- src/components/RoadmapContainer.jsx
+++ src/components/RoadmapContainer.jsx
@@ -1,14 +1,38 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import CompletedMilestones from "./CompletedMilestones";
 import Milestones from "./Milestones";
 import ProgressBar from "./ProgressBar";
+import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
 
 const RoadmapContainer = () => {
+  const [error, setError] = useState(null);
+  const [completedMilestones, setCompletedMilestones] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos", {
+        params: { _limit: 4, offset: 0, completed: true },
+      })
+      .then(({ data }) => setCompletedMilestones(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    // Print errors if any
+    return <div>An error occured: {error.message}</div>;
+  }
+
   return (
     <>
-      <CompletedMilestones />
-      <Milestones />
-      <ProgressBar />
+      <MDBContainer>
+        <MDBRow className="d-flex justify-content-center">
+          <CompletedMilestones milestones={completedMilestones} />
+          <Milestones />
+        </MDBRow>  
+        <ProgressBar />
+      </MDBContainer>
     </>
   );
 };
