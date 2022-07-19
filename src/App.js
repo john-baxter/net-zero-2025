56abab2c9b88e2d6ed526db1c88c1a3aab890e0f
Ewan Porter <porter.ewan@mail.com>
2022-07-19 13:03:49 +0100

    Co-authored-by: efblair <efblair@users.noreply.github.com> Co-authored-by: Fran Gordon <frangordon@users.noreply.github.com> Co-authored-by: IlyasBaqqari <IlyasBaqqari@users.noreply.github.com> Co-authored-by: George Musker <GMusk@users.noreply.github.com> Co-authored-by: paul-lawlor <paul-lawlor@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
 
     setBasicActive(value);
   };
+
   return (
     <div className="App">
       <Navbar />
--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { MDBContainer } from 'mdb-react-ui-kit';
+import DictionaryEntry from "./DictionaryEntry";
 
 const Dictionary = () => {
-  return (
-    <div>Dictionary</div>
-  )
-}
+  const [error, setError] = useState(null);
+  const [dictionary, setDictionary] = useState([]);
 
-export default Dictionary
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments", {
+        params: { _limit: 15, offset: 0 },
+      })
+      .then(({ data }) => setDictionary(data))
+      .catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    // Print errors if any
+    return <div>An error occured: {error.message}</div>;
+  }
+
+
+
+
+
+  return (
+    <MDBContainer  className="d-flex justify-content-center flex-row flex-wrap">
+      {dictionary.map(({ id, name, body }) => (
+
+        <DictionaryEntry id={id} term={name} definition={body}/>
+        
+
+     
+      ))}
+    </MDBContainer>
+  );
+};
+
+export default Dictionary;
